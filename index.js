1847afcfced7a979d09a8f7a9c604a2009dc2369
raminux <ramin.esmzad@hotmail.com>
2022-03-21 14:08:31 -0400

    adding some useful middlewares

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+
 const app = express()
 
 const PORT = 4000;
@@ -14,15 +19,12 @@ const handleHome = (req, res) => {
 const handleProfile = (req, res) => {
     res.send("You are on my profile");
 }
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan('dev'));
 
-const middleware = (req, res, next) => {
-    console.log("I'm the middleware!");
-    next();
-};
-
-app.use(middleware);
-
-// app.get("/", middleware, handleHome);
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
