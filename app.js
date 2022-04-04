aac5492e0602b5c9238d5d044fb8bd75acf84469
raminux <ramin.esmzad@hotmail.com>
2022-04-04 22:39:38 +0430

    Adding Express Router for user

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import { userRouter } from './router';
 
 const app = express()
 
@@ -22,8 +23,8 @@ app.use(helmet());
 app.use(morgan('dev'));
 
 app.get("/", handleHome);
-
 app.get("/profile", handleProfile);
+app.use("/user", userRouter);
 
 export default app;
 
