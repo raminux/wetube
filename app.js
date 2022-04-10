ccaceec50b2b2236cad1f793de0385c6c4357bcb
raminux <ramin.esmzad@hotmail.com>
2022-04-10 05:10:07 +0430

    Sending variables to views using Express locals middleware

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
 import globalRouter from './routers/globalRouter';
 import routes from './routes';
+import { localsMiddleware } from './middlewares';
 
 const app = express()
 
+// Middlewares
+app.use(helmet());
 
 // View
 app.set('view engine', 'pug');
@@ -18,8 +21,8 @@ app.set('view engine', 'pug');
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan('dev'));
+app.use(localsMiddleware);
 
 
 // Routers -> Controllers
