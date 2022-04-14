73219902e633a7ce2a830d2c51de64f78292e500
raminux <ramin.esmzad@hotmail.com>
2022-04-15 02:50:52 +0430

    Adding some fake videos to the home using mixins

---
+++ db.js
@@ -0,0 +1,16 @@
+// fake database to test app functionality
+export const fakeVideos = [
+    {
+        id: 112,
+        title: 'nice movie',
+        description: 'watch it yes',
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        views: 23,
+        user: {
+            id: 12,
+            name: 'ramin',
+            email: 'ramin@es.com',
+        }
+
+    }
+]
